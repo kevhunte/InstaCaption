0e1a6e1d08f82a14c5cceb1c066cebf621a32983
kevhunte <kevinmhunte@gmail.com>
2020-02-01 22:43:19 -0500

    Clean Up from previous implementation

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
-import Callback from '../views/Callback.vue'
 
 Vue.use(VueRouter)
 
@@ -18,12 +17,7 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  },
-  {
-      path: '/callback',
-      name: 'callback',
-      component: Callback
-    }
+  }
 ]
 
 const router = new VueRouter({
