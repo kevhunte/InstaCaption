aa25c763ff1b1a1ef7a87674ab24484a717940bc
kevhunte <kevinmhunte@gmail.com>
2020-02-06 21:55:28 -0500

    Completed Carousel on Homepage -- Next: About page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,7 @@
 <template>
 <div class="about">
   <h1>About page</h1>
+  <h3>add a parallax pic of the Bx as the background</h3>
   <h2>More nitty gritties</h2>
   <h2>Add shameless plugs here</h2>
 </div>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,34 +1,31 @@
 <template>
 <div class="home">
-  <h1>Home Page</h1>
+  <!--<h1 id="Homeheader" class="">InstaCaption</h1>-->
+  <!--Change font here-->
   <div id="carousel" class="row align-items-center">
-    <b-carousel id="carousel-1" class="w-responsive text-center col-md-6 mx-auto p-3 mt-2" v-model="slide" :interval="this.interval" indicators background="#ababab" img-width="1024" img-height="480" style="text-shadow: 1px 1px 2px #333;"
+    <b-carousel id="carousel-1" class="w-responsive col-md-7 mx-auto p-3 m-2" v-model="slide" :interval="this.interval" fade indicators background="#ababab" img-width="1024" img-height="480" style="text-shadow: 1px 1px 2px #333;"
       @sliding-start="onSlideStart" @sliding-end="onSlideEnd" @mouseenter="onPause" @mouseleave="onResume">
       <!-- Text slides with image -->
-      <b-carousel-slide caption="Your Favorite Artists" img-blank img-alt="Blank image"></b-carousel-slide>
+      <b-carousel-slide caption="Your Favorite Artists" :img-src="this.images[0]"></b-carousel-slide>
 
       <!-- Slides with custom text -->
-      <b-carousel-slide caption="And Their Lyrics" :img-src="this.images[0]">
-      </b-carousel-slide>
+      <b-carousel-slide caption="And Their Lyrics" :img-src="this.images[1]"></b-carousel-slide>
 
       <!-- Slides with image only -->
-      <b-carousel-slide caption="Caption Ready" img-blank img-alt="Blank image"></b-carousel-slide>
+      <b-carousel-slide caption="Caption Ready" :img-src="this.images[2]"></b-carousel-slide>
 
       <!-- Slides with img slot -->
       <!-- Note the classes .d-block and .img-fluid to prevent browser default image alignment -->
-      <b-carousel-slide caption="At An Instant" img-blank img-alt="Blank image">
-      </b-carousel-slide>
+      <b-carousel-slide caption="At An Instant" :img-src="this.images[3]"></b-carousel-slide>
 
       <!-- Slide with blank fluid image to maintain slide aspect ratio -->
-      <b-carousel-slide caption="Sign In And Enjoy" img-blank img-alt="Blank image">
-
+      <b-carousel-slide caption="Sign In And Enjoy" :img-src="this.images[4]">
       </b-carousel-slide>
+      <!--.carousel-caption css class to change font color -->
     </b-carousel>
   </div>
   <h2> A blurb about the site</h2>
-  <h2> Clarify that this is hiphop heavy</h2>
-  <h3>add a parallax pic of the Bx as the background</h3>
-  <h3>Maybe some advertisements too</h3>
+  <h3>Adsense div here</h3>
   <!--<img alt="Vue logo" src="../assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>-->
 </div>
@@ -47,7 +44,11 @@ export default {
       interval: this.timer,
       timer: 4000,
       images: [
-        require('@/assets/KungFuKenny.jpg')
+        require('@/assets/RoddyRicch.jpg'),
+        require('@/assets/AriLennox.png'),
+        require('@/assets/KungFuKenny.jpg'),
+        require('@/assets/Jhene.jpg'),
+        require('@/assets/DaBaby.jpg')
       ]
     }
   },
