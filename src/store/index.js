86de409ebf8d2f67756c96304e6e700c7e77cf2a
kevhunte <kevinmhunte@gmail.com>
2020-02-11 20:17:34 -0500

    Dummy REST API -- Next, work on toggling lyrics (accordion) -- Persisting data (local storage)

--- src/store/index.js
+++ src/store/index.js
@@ -5,28 +5,31 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    isAuthenticated: false,
-    profile: null,
-    token : null
+    previousSearchs: null
   },
   mutations: {
-    updateAuth(state, value){
-      console.log("updating auth state - "+value);
-      state.isAuthenticated = value
-    },
-    setProfile(state, data){
-      console.log("updating profile - "+data);
-      state.profile = data
-    },
-    setToken(state, value){
-      state.token = value
+    setPreviousSearches(state, value){
+      state.previousSearchs = value
     }
   },
   actions: {
+    async getPreviousSearch(context){
+      console.log('fetching previous searches..');
+      //where the fetch will be. Dummy data is stored on web server
+      const address = window.location.origin + '' + window.location.pathname;
+      //console.log("path returned:", address);
+      const response = await fetch(address + 'dummy.json'); //replace with API call to Gateway
+      /*
+      //Prod Version
+      const address = 'REST_API'+&userIDandProvider;
+      const response = await fetch(address);
+      */
+      const data = await response.json();
+      //console.log(data);
+      context.commit('setPreviousSearches',data); // set global value
+    }
   },
   getters: {
-    isAuthenticated: state => state.isAuthenticated,
-    getProfile: state => state.profile,
-    token: state => state.token
+    previousSearchs: state => state.previousSearchs
   }
 })
--- src/views/Captions.vue
+++ src/views/Captions.vue
@@ -3,8 +3,19 @@
   <h1 v-if="!$auth.isAuthenticated">Unauthenticated! Should not be seeing this page</h1>
   <h3>Welcome back {{$auth.user.given_name}}!</h3><br><br>
   <h4>Element to show last searched from API 2, GET Req.<br>
+    <div>
+      <h5>Here's what you searched last time you were here:</h5>
+      <div :id="'Song_'+ps.id" v-for="ps in this.$store.getters.previousSearchs" class="m-2">
+        <strong style="font-size:1.2rem;"> {{ps.name}} by {{ps.Artist}} </strong>
+        <b-button variant="info" pill size="sm">Show me the lyrics</b-button>
+        <br>
+        <!--Toggle with the button-->
+        <h6 style="font-size:1.2rem;">{{ps.Lyrics}}</h6>
+      </div>
+    </div>
     Check Cache then DB <br>
-    Store/Send back as much as possible, paginate data</h4><br><br>
+    Store/Send back as much as possible, paginate data
+  </h4><br><br>
   <h5>Form component for capturing search criteria<br>^^cleanse input for xss^^</h5><br><br>
   <h5>Pass to parent to create GET request to Genius wrapper, API 1. <br>
     Security: tarpit users that make more than 15 unique requests in a day.<br>
@@ -16,7 +27,22 @@
 export default {
   name: 'captions',
   data() {
-    return {}
+    return {
+      //previousSearchs: null
+    }
+  },
+  created() {
+    if (typeof this.$store.getters.previousSearchs !== undefined) {
+      this.getPreviousSearch();
+    }
+  },
+  methods: {
+    getPreviousSearch() {
+      this.$store.dispatch('getPreviousSearch');
+    },
+    showCaption(id) {
+      return this.$store.getters.previousSearchs[id];
+    }
   }
 }
 </script>
